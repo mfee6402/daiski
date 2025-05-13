b25250b48e0711e454fcf43ff40ff3fbab45632d
LuckyLise <c24680529@gmail.com>
2025-05-13 11:51:55 +0800

    完成第二頁切版

--- client/app/cart/layout.js
+++ client/app/cart/layout.js
@@ -3,10 +3,7 @@ import Container from '@/components/container';
 export default function CartLayout({ children }) {
   return (
     <>
-      {/* FIXME 測試用高度，之後h-[500]要刪掉 */}
-      <div className="relative h-[500]">
-        <Container>{children}</Container>
-      </div>
+      <Container>{children}</Container>
     </>
   );
 }
