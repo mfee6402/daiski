5a742b94f33b96c7ecfb3a4b5e6f811d916917d4
LuckyLise <c24680529@gmail.com>
2025-05-15 20:36:52 +0800

    修正prisma

--- server/routes/cart/index.js
+++ server/routes/cart/index.js
@@ -27,15 +27,14 @@ router.get('/', async function (req, res) {
   include: {
     // 相當於 JOIN cart ON cart.id = post.cart
     
-    CartProduct:{
-      include:{
-        product:{
-          select:{
-            name:true
-          }
-        }
-         
-      }
+     CartProduct: {
+      include: {
+        product: {
+          include: {
+            product_image: true, // 取得圖片
+          },
+        },
+      },
     },
     CartCourse:{
      select:{
@@ -54,22 +53,22 @@ router.get('/', async function (req, res) {
   },
 });
   // 攤平 CartProduct 內的 product.name 成 productName
-  const flatCartProduct = data.CartProduct.map(item => ({
-    id: item.id,
-    cartId: item.cartId,
-    productId: item.productId,
-    quantity: item.quantity,
-    productName: item.product.name
-  }));
+  // const flatCartProduct = data.CartProduct.map(item => ({
+  //   id: item.id,
+  //   cartId: item.cartId,
+  //   productId: item.productId,
+  //   quantity: item.quantity,
+  //   name: item.product.name
+  // }));
 
   // 替換原本的 CartProduct 為攤平後的資料
-  const cart = {
-    ...data,
-    CartProduct: flatCartProduct
-  };
+  // const cart = {
+  //   ...data,
+  //   CartProduct: flatCartProduct
+  // };
   res
     .status(200)
-    .json({ status: 'success', cart})
+    .json({ status: 'success', data})
 })
 
 // 更新(只有商品有數量，課程跟揪團票券固定只有1)
@@ -77,18 +76,18 @@ router.put('/:itemId', async function (req, res) {
   // console.log(req.body.data.cart)
   // console.log(req.body.data.cart.CartProduct[req.params.itemId].quantity)
 
-  const cart = await prisma.cartProduct.update({
+  // const cart = await prisma.cartProduct.update({
   // where: {
   //   id: +req.params.itemId,
   // },
   // data: {
   //   quantity: req.body.data.cart.CartProduct[req.params.itemId].quantity,
   // },
-});
-
+// });
+console.log(+req.params.itemId)
   res
     .status(200)
-    .json({ status: 'success',data:{cart}})
+    .json({ status: 'success',data:{}})
 })
 
 // 刪除
