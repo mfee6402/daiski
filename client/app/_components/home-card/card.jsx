b9d8ce6c7b63cc325f701cd00bcac579d73c2076
plusone602 <plustwohosiyo@gmail.com>
2025-05-16 11:38:38 +0800

    首頁完成

--- client/app/_components/home-card/card.jsx
+++ client/app/_components/home-card/card.jsx
@@ -3,40 +3,47 @@
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 
-export default function SkiCard({
+export default function Card({
   title = '',
   imgSrc = '',
-  src = '',
+  href = '',
   reverse = false,
   className = '',
+  textClassName = '',
 }) {
   return (
     <li
       className={[
-        'w-full h-full flex flex-row items-center',
+        'w-full h-auto flex flex-row items-center',
         reverse ? 'flex-row-reverse' : '',
-        className, // 傳入外部想加的 margin、負 margin…
+        className,
       ]
         .filter(Boolean)
         .join(' ')}
     >
       {/* 文字區塊 */}
-      <div className="w-[35%]">
-        <h3 className="text-2xl font-bold mb-4">{title}</h3>
-        <a href={src}>
-          <button className="bg-black text-white px-4 py-2 rounded">
-            了解更多
-          </button>
+      <div
+        className={['w-[35%] flex flex-col items-center', textClassName]
+          .filter(Boolean)
+          .join(' ')}
+      >
+        <h3 className="md:text-h5-tw text-p-tw  mb-4 font-tw">{title}</h3>
+        <a
+          href={href}
+          className="bg-black text-white md:px-4 md:py-2 p-1  font-tw text-p-tw hover:bg-white hover:text-black border-black border "
+        >
+          了解更多
         </a>
       </div>
 
       {/* 圖片區塊 */}
-      <div className="relative w-[65%] h-[35rem]">
+      <div className="relative w-[65%] xl:h-[40rem]">
         <Image
           src={imgSrc}
           alt={title}
-          fill
-          className={`object-cover h-full ${reverse ? 'mr-auto' : 'ml-auto'}`}
+          width={1000}
+          height={560}
+          className={`object-cover  ${reverse ? 'mr-auto' : 'ml-auto'}`}
           priority
         />
       </div>
