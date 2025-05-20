a74f44ac9fd0d9e243666d09e93998e64d136ff2
plusone602 <plustwohosiyo@gmail.com>
2025-05-20 15:58:17 +0800

    連接會員

--- server/routes/coupons/index.js
+++ server/routes/coupons/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import prisma from '../../lib/prisma.js';
+import { date } from 'zod';
 
 const router = express.Router();
 
@@ -41,8 +42,37 @@ router.get('/', async function (req, res) {
     amount: couponType.amount,
     target: couponTarget.target,
   }));
-
   res.status(200).json({ status: 'success', coupons });
 });
 
+// 會員領取優惠卷
+router.post('/user_coupon', async function (req, res) {
+  const { userId, couponId } = req.body;
+  try {
+    const claim = await prisma.userCoupon.create({
+      data: { userId, couponId },
+    });
+    res.status(200).json({ status: 'success', claim });
+  } catch (error) {
+    res.status(400).json({ error: '此優惠券已領取過' });
+  }
+});
+
+// 查詢某會員所有領取過的優惠券
+router.get('/user/coupons', async function (req, res) {
+  const userId = req.user.id;
+  console.log(userId);
+  // try {
+  //   const coupons = await prisma.userCoupon.create({
+  //     where: { userId },
+  //     include: { coupon: true },
+  //   });
+
+  //   coupons.map((usecoupon) => usecoupon.coupon);
+  //   res.status(200).json({ status: 'success', coupons });
+  // } catch (error) {
+  //   res.status(400).json({ error: '此優惠券已領取過' });
+  // }
+  res.status(200).json({ status: 'success', date: 'ok' });
+});
 export default router;
